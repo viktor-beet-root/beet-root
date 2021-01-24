fd179a7c317bd2d91c08466c7c6be0eadf64090c
oleksandrkirushov <alex.kirushov@gmail.com>
2021-01-24 22:27:21 +0200

    fixed bugs js_hw_06_object

--- students/kirushov_aleksandr/js_hw06_object_11_01/js/script.js
+++ students/kirushov_aleksandr/js_hw06_object_11_01/js/script.js
@@ -6,48 +6,59 @@ const car = {
     yearOfManufact: 2009,
     averageSpeed: 100,
 
-    getManufacturer() {
-        return "Производитель автомобиля завод: " + this.manufacturer;
-    },
-    getModel() {
-        return "Модель автомобиля: " + this.model;
-    },
-    getYearOfManufact() {
-        return "Год выпуска автомобиля: " + this.yearOfManufact;
-    },
-    getAverageSpeed() {
-        return "средння скорость автомобиля составляет: " + this.averageSpeed;
-    },
-
-    // Функция для вывода на экран информации об автомобиле;
-    getInformAboutCar() {
-        return this.getManufacturer() + '\n' + this.getModel() + '\n' + this.getYearOfManufact() + '\n' +
-            this.getAverageSpeed() + " км/ч";
-    },
-
-    // Функция для подсчета необходимого времени для преодоления переданного расстояния со средней скоростью.Учтите, что через каждые 4 часа дороги водителю необходимо делать перерыв на 1 час.
-    travelTime(distance) {
-        if (isNaN(distance) || distance <= 0 || distance === undefined) return "Ведено не допустимое значение";
-        let time = distance / this.averageSpeed;
-        let pause;
-        let answer;
-        if (time > 4) {
-            pause = ~~(time / 4);
-            time = time + pause;
-        };
-
-        if ((time % 10) === 1) {
-            answer = "час";
-        } else if ((time !== 12 && (time % 10) === 2) || ((time % 10) === 3 && time !== 13) || ((time % 10) === 4 && time !== 14)) {
-            answer = "часа";
-        } else {
-            answer = 'часов';
-        }
-
-        return 'Время необходимое для преодоления переданого расстояния, состовляет ' + time + ' ' + answer;
-
-    },
+    getManufacturer: getManufacturer,
+    getModel: getModel,
+    getYearOfManufact: getYearOfManufact,
+    getAverageSpeed: getAverageSpeed,
+    getInformAboutCar: getInformAboutCar,
+    travelTime: travelTime,
 }
+
+function getManufacturer() {
+    return "Производитель автомобиля завод: " + this.manufacturer;
+};
+
+function getModel() {
+    return "Модель автомобиля: " + this.model;
+};
+
+function getYearOfManufact() {
+    return "Год выпуска автомобиля: " + this.yearOfManufact;
+};
+
+function getAverageSpeed() {
+    return "средння скорость автомобиля составляет: " + this.averageSpeed;
+};
+
+// Функция для вывода на экран информации об автомобиле;
+function getInformAboutCar() {
+    return this.getManufacturer() + '\n' + this.getModel() + '\n' + this.getYearOfManufact() + '\n' +
+        this.getAverageSpeed() + " км/ч";
+};
+
+// Функция для подсчета необходимого времени для преодоления переданного расстояния со средней скоростью.Учтите, что через каждые 4 часа дороги водителю необходимо делать перерыв на 1 час.
+function travelTime(distance) {
+
+    if (isNaN(distance) || distance <= 0 || distance === undefined) return "Ведено не допустимое значение";
+    let time = distance / this.averageSpeed;
+    let pause;
+    let answer;
+
+    if (time > 4) {
+        pause = ~~(time / 4);
+        time = time + pause;
+    };
+
+    if ((time % 10) === 1) {
+        answer = "час";
+    } else if ((time !== 12 && (time % 10) === 2) || ((time % 10) === 3 && time !== 13) || ((time % 10) === 4 && time !== 14)) {
+        answer = "часа";
+    } else {
+        answer = 'часов';
+    }
+
+    return 'Время необходимое для преодоления переданого расстояния, состовляет ' + time + ' ' + answer;
+};
 
 console.log(car);
 console.log(car.getInformAboutCar());
@@ -85,19 +96,10 @@ function sumFraction(firstFraction, secondFraction) {
     const secondNumerator = firstFraction["denominator"] * secondFraction['numerator'];
     const commonDenomin = firstFraction["denominator"] * secondFraction['denominator'];
     const sum = firstNumerator + secondNumerator;
-    const newFraction = {
-        numerator: sum,
-        denominator: commonDenomin,
 
-        NOD: NOD,
-        getNumerator: getNumerator,
-        getDenominator: getDenominator,
-        getFraction: getFraction,
-    };
-    return newFraction;
+    return sum + '/' + commonDenomin;
 };
-
-console.log('Сумма дробей равна: ' + sumFraction(firstFraction, secondFraction).getFraction());
+console.log('Сумма дробей равна: ' + sumFraction(firstFraction, secondFraction));
 
 // Функция вычитания 2 - х объектов - дробей;
 
@@ -106,57 +108,35 @@ function subtractionFraction(firstFraction, secondFraction) {
     const secondNumerator = firstFraction["denominator"] * secondFraction['numerator'];
     const commonDenomin = firstFraction["denominator"] * secondFraction['denominator'];
     const subtraction = firstNumerator - secondNumerator;
-    if (subtraction !== 0) {
-        newFraction = {
-            numerator: subtraction,
-            denominator: commonDenomin,
 
-            NOD: NOD,
-            getNumerator: getNumerator,
-            getDenominator: getDenominator,
-            getFraction: getFraction,
-        };
-        return newFraction;
+    if (subtraction !== 0) {
+        return subtraction + '/' + commonDenomin;
     } else return 0;
 
 };
-console.log("Разница дробей равна: " + subtractionFraction(firstFraction, secondFraction).getFraction());
+console.log("Разница дробей равна: " + subtractionFraction(firstFraction, secondFraction));
 
 // Функци я умножения 2 - х объектов - дробей;
 
 function multiplicationFraction(firstFraction, secondFraction) {
-    const newFraction = {
-        numerator: firstFraction["numerator"] * secondFraction['numerator'],
-        denominator: firstFraction["denominator"] * secondFraction['denominator'],
+    const newNumerator = firstFraction["numerator"] * secondFraction['numerator'];
+    const newDenominator = firstFraction["denominator"] * secondFraction['denominator'];
 
-        NOD: NOD,
-        getNumerator: getNumerator,
-        getDenominator: getDenominator,
-        getFraction: getFraction,
-    };
-    return newFraction;
+    return newNumerator + '/' + newDenominator;
 };
 
-console.log("Произведение дробей равно: " + multiplicationFraction(firstFraction, secondFraction).getFraction());
+console.log("Произведение дробей равно: " + multiplicationFraction(firstFraction, secondFraction));
 
 // Функция деления 2 - х объектов - дробей;
 
 function divisionFraction(firstFraction, secondFraction) {
-    const newFraction = {
-        numerator: firstFraction["numerator"] * secondFraction['denominator'],
-        denominator: firstFraction["denominator"] * secondFraction['numerator'],
+    const newNumerator = firstFraction["numerator"] * secondFraction['denominator'];
+    const newDenominator = firstFraction["denominator"] * secondFraction['numerator'];
 
-        NOD: NOD,
-        getNumerator: getNumerator,
-        getDenominator: getDenominator,
-        getFraction: getFraction,
-        reductionFraction: reductionFraction,
-    };
-
-    return newFraction;
+    return newNumerator + '/' + newDenominator;
 };
 
-console.log("Деление дробей равно: " + divisionFraction(firstFraction, secondFraction).getFraction());
+console.log("Деление дробей равно: " + divisionFraction(firstFraction, secondFraction));
 
 // Функция сокращения объекта - дроби.
 
@@ -164,6 +144,7 @@ function reductionFraction(fraction) {
     const nod = fraction.NOD();
     let newNumerator = 0;
     let newDenominator = 0;
+
     if (nod === 1) {
         newNumerator = fraction.numerator;
         newDenominator = fraction.denominator;
@@ -171,15 +152,8 @@ function reductionFraction(fraction) {
         newNumerator = fraction.numerator / nod;
         newDenominator = fraction.denominator / nod;
     }
-    newFraction = {
-        numerator: newNumerator,
-        denominator: newDenominator,
 
-        getNumerator: getNumerator,
-        getDenominator: getDenominator,
-        getFraction: getFraction,
-    }
-    return newFraction;
+    return newNumerator + '/' + newDenominator;
 };
 
 function NOD() {
@@ -208,86 +182,85 @@ function getFraction() {
     return this.getNumerator() + '/' + this.getDenominator();
 };
 
-console.log(reductionFraction(sumFraction(firstFraction, secondFraction)).getFraction());
-console.log(reductionFraction(subtractionFraction(firstFraction, secondFraction)).getFraction());
-console.log(reductionFraction(multiplicationFraction(firstFraction, secondFraction)).getFraction());
-console.log(reductionFraction(divisionFraction(firstFraction, secondFraction)).getFraction());
-100
+console.log(reductionFraction(firstFraction));
+
 
 console.log('Задание 3. Создать объект, описывающий время (часы, минуты, секунды), и следующие функции для работы с этим объектом:');
 const time = {
     hours: 0,
     minuts: 0,
     seconds: 0,
+};
 
-    // Функция вывода времени на экран;
-    getTime: function () {
-        const newHour = doubleNumber(this.hours);
-        const newMinuts = doubleNumber(this.minuts);
-        const newSeconds = doubleNumber(this.seconds);
-        return newHour + ':' + newMinuts + ':' + newSeconds;
-    },
+// Функция вывода времени на экран;
+function getTime() {
+    const newHour = doubleNumber(time.hours);
+    const newMinuts = doubleNumber(time.minuts);
+    const newSeconds = doubleNumber(time.seconds);
+    return newHour + ':' + newMinuts + ':' + newSeconds;
+};
 
-    // Функция изменения времени на переданное количество секунд;
+// Функция изменения времени на переданное количество секунд;
 
-    changeSeconds: function (sec) {
-        if (sec === undefined || sec < 0) return 'К сожалению, время не возможно вернуть назад))';
-        const hSec = 3600;
-        const mSec = 60;
-        let newSec = sec;
-        this.hours = ~~(newSec / hSec) + this.hours;
+function changeSeconds(sec) {
+    if (sec === undefined || sec < 0) return 'К сожалению, время не возможно вернуть назад))';
+    const hSec = 3600;
+    const mSec = 60;
+    let newSec = sec;
+    time.hours = ~~(newSec / hSec) + time.hours;
 
-        if (this.hours > 24 || this.hours % 24 === 0) this.hours = this.hours - (24 * (~~(this.hours / 24)));
+    if (time.hours > 24 || time.hours % 24 === 0) time.hours = time.hours - (24 * (~~(time.hours / 24)));
 
-        newSec = newSec % hSec;
-        this.minuts = ~~(newSec / mSec) + this.minuts;
+    newSec = newSec % hSec;
+    time.minuts = ~~(newSec / mSec) + time.minuts;
 
-        if (this.minuts > 60 || this.minuts % 60 === 0) {
-            this.minuts = this.minuts - (60 * (~~(this.minuts / 60)));
-            this.hours++;
-            if (this.hours >= 24 || this.hours % 24 === 0) this.hours = this.hours - (24 * (~~(this.hours / 24)));
+    if (time.minuts > 60 || time.minuts % 60 === 0) {
+        time.minuts = time.minuts - (60 * (~~(time.minuts / 60)));
+        time.hours++;
+        if (time.hours >= 24 || time.hours % 24 === 0) time.hours = time.hours - (24 * (~~(time.hours / 24)));
+    }
+
+    time.seconds = newSec % mSec + time.seconds;
+
+    if (time.seconds === 60) {
+        time.seconds = time.seconds - (60 * (~~(time.seconds / 60)));
+        time.minuts++
+
+        if ((time.minuts > 60 || time.minuts % 60 === 0)) {
+            time.minuts = time.minuts - (60 * (~~(time.minuts / 60)));
+            time.hours++;
+
+            if (time.hours > 24 || time.hours % 24 === 0) time.hours = time.hours - (24 * (~~(time.hours / 24)));
         }
+    }
+};
 
-        this.seconds = newSec % mSec + this.seconds;
+//Функция изменения времени на переданное количество минут;
 
-        if (this.seconds === 60) {
-            this.seconds = this.seconds - (60 * (~~(this.seconds / 60)));
-            this.minuts++
+function changeMinuts(min) {
 
-            if ((this.minuts > 60 || this.minuts % 60 === 0)) {
-                this.minuts = this.minuts - (60 * (~~(this.minuts / 60)));
-                this.hours++;
+    if (min === undefined || min < 0) return 'К сожалению, время не возможно вернуть назад))';
+    const hMin = 60;
+    time.hours = ~~(min / hMin) + time.hours;
 
-                if (this.hours > 24 || this.hours % 24 === 0) this.hours = this.hours - (24 * (~~(this.hours / 24)));
-            }
-        }
-    },
+    if (time.hours > 24 || time.hours % 24 === 0) time.hours = time.hours - (24 * (~~(time.hours / 24)));
 
-    //Функция изменения времени на переданное количество минут;
+    time.minuts = min % hMin + time.minuts;
 
-    changeMinuts: function (min) {
-        if (min === undefined || min < 0) return 'К сожалению, время не возможно вернуть назад))';
-        const hMin = 60;
-        this.hours = ~~(min / hMin) + this.hours;
+    if (time.minuts > 60 || time.minuts % 60 === 0) {
+        time.minuts = time.minuts - (60 * (~~(time.minuts / 60)));
+        time.hours++;
+        if (time.hours >= 24 && time.hours % 24 === 0) time.hours = time.hours - (24 * (~~(time.hours / 24)));
+    }
+};
 
-        if (this.hours > 24 || this.hours % 24 === 0) this.hours = this.hours - (24 * (~~(this.hours / 24)));
+//Функция изменения времени на переданное количество минут;
 
-        this.minuts = min % hMin + this.minuts;
+function changeHours(hour) {
 
-        if (this.minuts > 60 || this.minuts % 60 === 0) {
-            this.minuts = this.minuts - (60 * (~~(this.minuts / 60)));
-            this.hours++;
-            if (this.hours >= 24 && this.hours % 24 === 0) this.hours = this.hours - (24 * (~~(this.hours / 24)));
-        }
-    },
-
-    //Функция изменения времени на переданное количество минут;
-
-    changeHours: function (hour) {
-        if (hour === undefined || hour < 0) return 'К сожалению, время не возможно вернуть назад))';
-        this.hours = hour + this.hours;
-        if (this.hours >= 24 || this.hours % 24 === 0) this.hours = this.hours - (24 * (~~(this.hours / 24)));
-    },
+    if (hour === undefined || hour < 0) return 'К сожалению, время не возможно вернуть назад))';
+    time.hours = hour + time.hours;
+    if (time.hours >= 24 || time.hours % 24 === 0) time.hours = time.hours - (24 * (~~(time.hours / 24)));
 };
 
 function doubleNumber(params) {
@@ -297,10 +270,10 @@ function doubleNumber(params) {
 };
 
 console.log(time);
-console.log(time.getTime());
-time.changeSeconds(3661);
-console.log(time.getTime());
-time.changeMinuts(65);
-console.log(time.getTime());
-time.changeHours(22);
-console.log(time.getTime());
+console.log(getTime());
+changeSeconds(3661);
+console.log(getTime());
+changeMinuts(65);
+console.log(getTime());
+changeHours(22);
+console.log(getTime());
